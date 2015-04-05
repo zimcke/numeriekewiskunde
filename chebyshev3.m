e37fc624d9c23b7d4de0fd2504310369064dacd5
CrombezSarah <crombez.sarah14@gmail.com>
2015-04-05 20:48:53 +0200

    chebyshev
    er zat nog een foutje in

--- chebyshev3.m
+++ chebyshev3.m
@@ -11,11 +11,9 @@ for i=1:length(x)
             dT(i,j)=1;
         else
             T(i,j)=2*x(i)*T(i,j-1)-T(i,j-2);
-            dT(i,j)=2*x(i)*dT(i,j-1)+2*T(i,j-1)-dT(j-2);
+            dT(i,j)=2*x(i)*dT(i,j-1)+2*T(i,j-1)-dT(i,j-2);
         end
     end
 end
-T
-dT
 end
 
