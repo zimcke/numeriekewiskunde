1112ba55dd249db4f9aa16587254790734671229
zimcke <zimcke@youngmusic.org>
2015-04-06 15:00:22 +0200

    Chebychev polynomials
    - Renamed chebyshev3.m to chebyshev.m
    - Removed chebyshev2.m and chebyshev2.m
    - Add documentation to chebychev.m
    
    TODO: update test class

--- chebyshev.m
+++ chebyshev.m
@@ -1,32 +1,34 @@
-function [ T, dT ] = chebyshev( x,n )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-T = double(zeros(length(x),n))
-dT = double(zeros(length(x),n))
+% input x = the horizontal points in which the Chebychev polynomial are to be evaluated
+% input n = the maximum degree to which to compute the interpolants
+% output T = the Chebyshev interpolants in all x values, to degree n
+% output dT = derivative of the Chebychev interpolants in all x values, to
+% degree n
+function [ T , dT ] = chebyshev( x , n )
+T = zeros(length(x), n+1);
+dT = zeros(length(x), n+1);
 
-if(n == 0)
-    for j = drange(0:n)
-        for i = drange(1:size(x))
-            T(i,j) = 1;
-            dT(i,j) = 0;
+% iterate through all x values
+for i = 1:length(x)
+    
+    % iteratre through all degrees
+    for j = 1:n+1
+        
+        % if degree == 0
+        if j==1
+            T(i,j)=1;
+            dT(i,j)=0;
+            
+        % if degree == 2    
+        elseif j==2;
+            T(i,j)=x(i);
+            dT(i,j)=1;
+               
+        else
+            % recursion formula
+            T(i,j)=2*x(i)*T(i,j-1)-T(i,j-2);
+            dT(i,j)=2*x(i)*dT(i,j-1)+2*T(i,j-1)-dT(i,j-2);
         end
     end
-end
-if(n == 1)
-     for j = drange(n:0)
-        for i = drange(size(x):1)
-            T(i,j) = x(i);
-            dT(i,j) = 1;
-        end
-     end
-else
-    for j = drange(n:0)
-        for i = drange(size(x):1)
-               T(i,j) = 2*x*chebyshev(x,j-1) - chebyshev(x,j-2);
-               dT(i,j) = 1;
-        end
-    end
-
 end
 end
 
