eb9585b4c16f14fd8b6869a9563e6063791d74c1
zimcke <zimcke@youngmusic.org>
2015-04-06 15:40:57 +0200

    main.m
    - Renamed deel1.m to main.m
    - Made different sections in our testprogram: deel 1, deel 2, deel 3

--- deel1.m
+++
@@ -1,6 +0,0 @@
-
-clc
-x = linspace(-1,1,10);
-c = [1:10]
-[T, dT] = chebyshev3(x,5)
-[y, dy] = chebpolyval(c,x)
---
+++ main.m
@@ -0,0 +1,11 @@
+%% Deel 1: Drie veeltermbasissen
+clc
+format long
+x = linspace(-1,1,10);
+c = [1:10]
+[T, dT] = chebyshev(x,5)
+%[y, dy] = chebpolyval(c,x)
+
+%% Deel 2: Veelterminterpolatie
+
+%% Deel 3: Methode van Newton-Raphson
