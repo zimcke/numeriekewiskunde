1c5807e0b87d624cf742d94d206f1b2345851a13
zimcke <zimcke@youngmusic.org>
2015-04-06 17:59:52 +0200

    Try to complete chebpolyval.m + evalueer_lagrange.m
    I don't know if this is correct.

---
+++ evalueer_lagrange.m
@@ -0,0 +1,21 @@
+function [ y ] = evalueer_lagrange( x, f, z )
+
+% initialize y
+y = 0;
+
+% iterate through all points
+for i = 1:length(x)
+    t = f(i);
+    
+    for j = 1:lenght(x)
+        
+        if i ~= j
+           t = t .* (z - x(j))/(x(i)-x(j));
+        end
+    end
+    
+    y = y + t;
+end    
+
+end
+
--- monomiaal.m
+++ monomiaal.m
@@ -1,6 +1,15 @@
+% input x = the horizontal points in which the monomial polynomial are to be evaluated
+% input n = the maximum degree to which to compute the interpolants
+% output M = the monomial interpolants in all x values, to degree n
 function [ M ] = monomiaal( x,n )
+
+% initialize M
 M=zeros(length(x),n+1);
+
+% iterate through horizontal points
 for i=1:length(x)
+    
+    % iterate through all degrees
     for j=1:n+1
         M(i,j)=x(i)^(j-1);
     end
