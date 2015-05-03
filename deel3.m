f5641fb8914d3fe8d16aea68af9c11dd4263cdd4
zimcke <zimcke@youngmusic.org>
2015-05-03 17:41:00 +0200

    Final report + code

--- deel3.m
+++ deel3.m
@@ -1,17 +1,17 @@
 %% Deel 3: Methode van Newton-Raphson
 
-%variabelen toekennen
+% Initialize variables
 format long
 load('cheb_coeffs.mat')
 nbpoints = 10000;
 tol = 10^-50;
 nmax = 100;
 h = [10^-1 10^-1.5 10^-2 10^-3 10^-6];
-x_start = linspace(-1,1,100);
+x_start = linspace(-1,1,nmax);
 x = linspace(-1,1,nbpoints);
 [y,dy] = chebpolyval(c,x);
 
-%nulpunten benaderen
+% Nulpunten benaderen
 clearvars nulp
 N_R = newton_cheb(c,x_start(1),tol,nmax,'exact',h);
 nulp = N_R(end);
@@ -29,13 +29,15 @@ title('Chebyshevveelterm met nulpunten')
 legend('Chebyshevveelterm','nulpunten met Newton-Raphson benadering')
 xlabel('x-waarden in [-1,1]') % x-axis label
 ylabel('Chebyshevveelterm') % y-axis label
+print('report\figuur_1','-depsc');
 
 %plot: residu's i.f.v. de startwaarden
 figure(2)
 plot(x_start,nulp,'.')
-title('residus i.f.v. de startwaarden')
-xlabel('startwaarden') % x-axis label
-ylabel('nulpunten met Newton-Raphson benadering') % y-axis label
+title('Residu''s i.f.v. de startwaarden')
+xlabel('Startwaarden') % x-axis label
+ylabel('Nulpunten met Newton-Raphson benadering') % y-axis label
+print('report\figuur_2','-depsc');
 
 %plot: raaklijn aan het punt waar de veelterm zijn absolute maximum bereikt
 x_max = x(find(y==max(y)));
@@ -43,13 +45,14 @@ x_max = x(find(y==max(y)));
 raaklijn = y_max+dy_max.*(x-x_max);
 figure(3)
 plot(x,y,x,raaklijn)
-title('raaklijn aan het absolute maximum')
+title('Raaklijn aan het absolute maximum')
 legend('Chebyshevveelterm','raaklijn')
 xlabel('x-waarden in [-1,1]') % x-axis label
 ylabel('Chebyshevveelterm') % y-axis label
 
 
 %plot de fout i.f.v. de iteratiestap voor startwaarde x0=-0.9, en x0=0.5
+nmax = 30;
 x_0 = [-0.9,0.5];
 x_nul = zeros(length(x_0),nmax);
 nulp_ben = zeros(length(x_0),nmax);
@@ -60,13 +63,28 @@ end
 x_stap = (1:nmax);
 figure(4)
 semilogy(x_stap,abs(nulp_ben))
-title('fout i.f.v de iteratiestap')
-legend('x0=-0.9','x0=0.5')
-xlabel('iteratiestap') % x-axis label
-ylabel('fout') % y-axis label
+title('Fout in functie van de iteratiestap')
+legend('x0 = -0.9','x0 = 0.5')
+xlabel('Iteratiestap') % x-axis label
+ylabel('Fout') % y-axis label
+print('report\figuur_3','-depsc');
 
 %plot: fout i.f.v. de iteratiestap voor startwaarde x0=0.5 en h
-nulp_exact = nulp_ben(2,:);
+nmax = 10;
+clearvars x_stap;
+clearvars x_nul;
+clearvars nulp_ben;
+x_stap = (1:nmax);
+
+x_0 = [-0.9,0.5];
+x_nul = zeros(length(x_0),nmax);
+nulp_ben = zeros(length(x_0),nmax);
+for k = 1:length(x_0)
+    x_nul(k,:) = newton_cheb(c,x_0(k), tol, nmax, 'exact', h);
+    nulp_ben(k,:) = chebpolyval(c,x_nul(k,:));
+end
+
+nulp_exact = nulp_ben(2,nmax);
 x_diff = zeros(length(h),nmax);
 nulp_diff = zeros(length(h),nmax);
 for k = 1:length(h)
@@ -76,8 +94,9 @@ end
 
 figure(5)
 semilogy(x_stap,abs(nulp_exact),x_stap,abs(nulp_diff))
-title('fout i.f.v de iteratiestap')
-legend('exact','h=10^-1','h=10^-1.5','h=10^-2','h=10^-3','h=10^-6')
-xlabel('iteratiestap') % x-axis label
-ylabel('fout') % y-axis label
+title('Fout in functie van de iteratiestap')
+legend('exact','h = 10^-1','h = 10^-1.5','h = 10^-2','h = 10^-3','h = 10^-6')
+xlabel('Iteratiestap') % x-axis label
+ylabel('Fout') % y-axis label
+print('report\figuur_4','-depsc');
     
